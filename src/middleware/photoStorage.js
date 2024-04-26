09a072b64b51bb21adb06a26abdd8c9909107293
gilberthappi <gdushimimana6@gmail.com>
2024-04-26 11:46:36 +0200

    Update event assets

--- src/middleware/photoStorage.js
+++ src/middleware/photoStorage.js
@@ -3,12 +3,12 @@ import multer from 'multer';
 
 export const storage = multer.diskStorage({
   destination(req, file, cb) {
-    cb(null, 'tour_assets/');
+    cb(null, 'event_assets/');
   },
   filename(req, file, cb) {
     cb(null, Date.now()+'-'+ file.originalname);
   },
 });
 
-const upload = multer({ dest: 'tour_assets/', storage: storage });
+const upload = multer({ dest: 'event_assets/', storage: storage });
 export const uploaded = upload.fields([{name: 'backdropImage', maxCount: 1}, {name: 'Gallery', maxCount: 8}]);
