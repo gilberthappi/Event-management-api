45468d1f8791dc879d13083cb320938a9ebb3022
gilberthappi <gdushimimana6@gmail.com>
2024-04-28 12:09:03 +0200

    Update swagger documentation

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,8 +2,8 @@ import express from 'express';
 import authRouter from './authentication';
 import eventRouter from './event';
 import bookingRouter from './booking';
-import contactRouter from './contacts';
-import PaymentRoute from './payment';
+// import contactRouter from './contacts';
+// import PaymentRoute from './payment';
 
 
 const mainRouter = express.Router();
@@ -11,7 +11,7 @@ const mainRouter = express.Router();
 mainRouter.use('/auth', authRouter);
 mainRouter.use('/event', eventRouter);
 mainRouter.use('/booking', bookingRouter);
-mainRouter.use('/cont', contactRouter);
-mainRouter.use('/pay', PaymentRoute);
+// mainRouter.use('/cont', contactRouter);
+// mainRouter.use('/pay', PaymentRoute);
 
 export default mainRouter;
